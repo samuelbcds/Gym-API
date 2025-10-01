9c2668e7298c35af08a1a06f4786864ce95414a5
Samuel <baltaz.samuel@gmail.com>
2025-10-01 00:57:29 -0300

    feat: implement `user.repository` file for Prisma interactions

---
+++ src/repository/user.repository.ts
@@ -0,0 +1,44 @@
+import { PrismaClient, Prisma, $Enums } from "@prisma/client";
+
+
+const prisma = new PrismaClient({
+    omit: {
+        user: {
+            id: true,
+            isActive: true,
+            hash: true,
+            salt: true
+        }
+    }
+});
+
+class UserRepository {
+    async createUser(data: Prisma.UserCreateInput) {
+        return prisma.user.create({ data });
+    }
+
+    async findUsersByRole(role?: $Enums.UserRole) {
+        return prisma.user.findMany({
+            select: {
+                name: true,
+                email: true,
+            },
+            where: role ? { role } : undefined,
+        });
+    }
+
+    async updateUserById(userId: number, data: Prisma.UserUpdateInput) {
+        return prisma.user.update({ where: { id: userId }, data });
+    }
+
+    async softDeleteUser(userId: number) {
+        return prisma.user.update({
+            where: { id: userId },
+            data: {
+                isActive: false,
+            },
+        });
+    }
+}
+
+export default new UserRepository();
