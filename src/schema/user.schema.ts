ecb1d7d8561db36293f41828e080073b2cc0926c
Samuel <baltaz.samuel@gmail.com>
2025-10-09 00:12:59 -0300

    feat(user): update user schema with new validation properties
    Adds getUserById and searchUsers methods to the UserSchema.
    Refactors role validation to use $Enums.UserRole for consistency
    and flexibility in user role management.

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,3 +1,4 @@
+import { $Enums } from "@prisma/client";
 import { z } from "zod";
 class UserSchema {
   create = z.object({
@@ -6,12 +7,28 @@ class UserSchema {
     name: z.string().min(2).max(100),
   });
 
+  getUserById = z.object({
+    userId: z.uuid(),
+  });
+
   list = z.object({
-    role: z.enum(["VISITOR", "CUSTOMER", "PERSONAL", "ADMIN"]).optional(),
+    role: z.enum($Enums.UserRole).optional(),
   });
 
   update = z.object({
     name: z.string().min(2).max(100).optional(),
+  });
+
+  searchUsers = z.object({
+    role: z.enum($Enums.UserRole).optional(),
+    name: z.string().optional(),
+    email: z.string().optional(),
+    isActive: z.coerce.boolean().optional(),
+    havePhone: z.string().optional(),
+    createdAfter: z.string().optional(),
+    createdBefore: z.string().optional(),
+    page: z.number().int().min(1).optional(),
+    limit: z.number().int().min(1).max(100).optional(),
   });
 }
 
