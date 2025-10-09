9eb18c536bbf3fae536ff7e97994c5721c133dc4
Samuel <baltaz.samuel@gmail.com>
2025-10-09 00:07:56 -0300

    feat(service): implement searchUsers method for user filtering
    Adds the searchUsers method to UserService, allowing for user
    searching based on various filters such as role, name, email,
    and account status. This enhances user management capabilities
    and improves the overall functionality of the service.

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,14 +1,89 @@
+import userRepository from "../repository/user.repository";
 import { ForbiddenError, AuthenticationError } from "../utils/errors";
+import { Prisma, $Enums } from "@prisma/client";
 
 class UserService {
-  checkIsAdmin(token: { id: string; role: string } | null) {
+  searchUsers(
+    token: { id: string; role: string },
+    filters: {
+      role?: $Enums.UserRole;
+      name?: string;
+      email?: string;
+      isActive?: boolean;
+      havePhone?: boolean;
+      createdAfter?: Date;
+      createdBefore?: Date;
+      page?: number;
+      limit?: number;
+    }
+  ) {
+    this.checkIsAdmin(token);
+
+    const {
+      role,
+      name,
+      email,
+      isActive = true,
+      havePhone,
+      createdAfter,
+      createdBefore,
+      page = 1,
+      limit = 20,
+    } = filters;
+
+    const whereCondition: Prisma.UserWhereInput = {};
+
+    if (role) whereCondition.role = role;
+    if (name) whereCondition.name = { contains: name, mode: "insensitive" };
+    if (email) whereCondition.email = { contains: email, mode: "insensitive" };
+    if (isActive !== undefined) whereCondition.isActive = isActive;
+    if (havePhone) whereCondition.phone = { not: { equals: null } };
+
+    if (createdAfter || createdBefore) {
+      whereCondition.createdAt = {};
+
+      if (createdAfter) {
+        whereCondition.createdAt.gte = createdAfter;
+      }
+      if (createdBefore) {
+        whereCondition.createdAt.lte = createdBefore;
+      }
+    }
+
+    const skip = (page - 1) * limit;
+    const take = limit;
+
+    const orderBy = { name: "asc" as Prisma.SortOrder };
+
+    return userRepository.findUsers({
+      whereCondition,
+      skip,
+      take,
+      orderBy,
+    });
+  }
+
+  checkIsSelfOrAdmin(token: { id: string; role: string }, userId: string) {
+    if (!token || (token.id !== userId && token.role !== "ADMIN")) {
+      throw new ForbiddenError("Access denied");
+    }
+    return true;
+  }
+  checkIsAdmin(token: { id: string; role: string }) {
     if (!token || token.role !== "ADMIN") {
       throw new ForbiddenError("Access denied");
     }
     return true;
   }
 
-  checkIsAuthenticated(token: { id: string; role: string } | null) {
+  checkIsPersonalTrainerOrAdmin(token: { id: string; role: string }) {
+    if (!token || (token.role !== "ADMIN" && token.role !== "PERSONAL")) {
+      throw new ForbiddenError("Access denied");
+    }
+    return true;
+  }
+
+  checkIsAuthenticated(token: { id: string; role: string }) {
     if (!token) {
       throw new AuthenticationError("Access denied");
     }
